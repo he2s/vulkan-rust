c8eb090797889254019d6019239d47abede0d0c8
Henning Schmick <henning.schmick@gmail.com>
2025-09-04 22:20:54 +0200

    Cool shader.

---
+++ resources/vulkan-tutorial-rs/src/fs.rs
@@ -0,0 +1,13 @@
+use std::io::Cursor;
+use std::path::Path;
+
+pub fn load<P: AsRef<Path>>(path: P) -> Cursor<Vec<u8>> {
+    use std::fs::File;
+    use std::io::Read;
+
+    let mut buf = Vec::new();
+    let fullpath = Path::new("../assets").join(path);
+    let mut file = File::open(fullpath).unwrap();
+    file.read_to_end(&mut buf).unwrap();
+    Cursor::new(buf)
+}
